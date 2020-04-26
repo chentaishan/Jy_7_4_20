58a7c3469e99eac03a56a38a615bbe23d487140f
chen <xxx@example.com>
2020-04-26 08:20:09 +0800

    实现微博分享

--- testyoumeng/src/main/java/com/example/testyoumeng/MainActivity.java
+++ testyoumeng/src/main/java/com/example/testyoumeng/MainActivity.java
@@ -1,13 +1,135 @@
 package com.example.testyoumeng;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.dfqin.grantor.PermissionListener;
+import com.github.dfqin.grantor.PermissionsUtil;
+import com.umeng.socialize.ShareAction;
+import com.umeng.socialize.UMShareAPI;
+import com.umeng.socialize.UMShareListener;
+import com.umeng.socialize.bean.SHARE_MEDIA;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mClick1;
+    private Button mClick2;
+    private Button mClick3;
+    private Button mClick;
+    String[] permissions = {
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.INTERNET,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_WIFI_STATE
+
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        PermissionsUtil.requestPermission(this, new PermissionListener(){
+
+            @Override
+            public void permissionGranted(@NonNull String[] permission) {
+
+            }
+
+            @Override
+            public void permissionDenied(@NonNull String[] permission) {
+
+            }
+        },permissions);
+        initView();
     }
+
+    private void initView() {
+        mClick1 = (Button) findViewById(R.id.click1);
+        mClick1.setOnClickListener(this);
+        mClick2 = (Button) findViewById(R.id.click2);
+        mClick2.setOnClickListener(this);
+        mClick3 = (Button) findViewById(R.id.click3);
+        mClick3.setOnClickListener(this);
+        mClick = (Button) findViewById(R.id.click);
+        mClick.setOnClickListener(this);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.click:
+                // TODO 20/04/25
+                new ShareAction(MainActivity.this).withText("hello").setDisplayList(SHARE_MEDIA.SINA,SHARE_MEDIA.QQ,SHARE_MEDIA.WEIXIN)
+                        .setCallback(shareListener).open();
+                break;
+             case R.id.click1:
+                // TODO 20/04/25
+                break;
+            case R.id.click2:
+                // TODO 20/04/25
+                break;
+            case R.id.click3:
+                // TODO 20/04/25
+                break;
+            default:
+                break;
+        }
+    }
+
+    private UMShareListener shareListener = new UMShareListener() {
+        /**
+         * @descrption 分享开始的回调
+         * @param platform 平台类型
+         */
+        @Override
+        public void onStart(SHARE_MEDIA platform) {
+
+        }
+
+        /**
+         * @descrption 分享成功的回调
+         * @param platform 平台类型
+         */
+        @Override
+        public void onResult(SHARE_MEDIA platform) {
+            Toast.makeText(MainActivity.this,"成功  了",Toast.LENGTH_LONG).show();
+        }
+
+        /**
+         * @descrption 分享失败的回调
+         * @param platform 平台类型
+         * @param t 错误原因
+         */
+        @Override
+        public void onError(SHARE_MEDIA platform, Throwable t) {
+            Toast.makeText(MainActivity.this,"失    败"+t.getMessage(),Toast.LENGTH_LONG).show();
+        }
+
+        /**
+         * @descrption 分享取消的回调
+         * @param platform 平台类型
+         */
+        @Override
+        public void onCancel(SHARE_MEDIA platform) {
+            Toast.makeText(MainActivity.this,"取消   了",Toast.LENGTH_LONG).show();
+
+        }
+    };
 }
--- testyoumeng/src/main/java/com/example/testyoumeng/MyApp.java
+++ testyoumeng/src/main/java/com/example/testyoumeng/MyApp.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.commonsdk.UMConfigure;
+import com.umeng.socialize.PlatformConfig;
 
 public class MyApp extends Application {
     @Override
@@ -19,5 +20,23 @@ public class MyApp extends Application {
 
         UMConfigure.setLogEnabled(true);
         MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.AUTO);
+
+
+        PlatformConfig.setWeixin("wxdc1e388c3822c80b", "3baf1193c85774b3fd9d18447d76cab0");
+        //豆瓣RENREN平台目前只能在服务器端配置
+        PlatformConfig.setSinaWeibo("283601501", "3e5e999fd26d28eef0100575b0170469","http://sns.whalecloud.com");
+//        PlatformConfig.setYixin("yxc0614e80c9304c11b0391514d09f13bf");
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+//        PlatformConfig.setTwitter("3aIN7fuF685MuZ7jtXkQxalyi", "MK6FEYG63eWcpDFgRYw4w9puJhzDl0tyuqWjZ3M7XJuuG7mMbO");
+//        PlatformConfig.setAlipay("2015111700822536");
+//        PlatformConfig.setLaiwang("laiwangd497e70d4", "d497e70d4c3e4efeab1381476bac4c5e");
+//        PlatformConfig.setPinterest("1439206");
+//        PlatformConfig.setKakao("e4f60e065048eb031e235c806b31c70f");
+//        PlatformConfig.setDing("dingoalmlnohc0wggfedpk");
+//        PlatformConfig.setVKontakte("5764965","5My6SNliAaLxEm3Lyd9J");
+//        PlatformConfig.setDropbox("oz8v5apet3arcdy","h7p2pjbzkkxt02a");
+//        PlatformConfig.setYnote("9c82bf470cba7bd2f1819b0ee26f86c6ce670e9b");
+
+
     }
 }
