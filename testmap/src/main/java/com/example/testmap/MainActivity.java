7858d2cb9c29658ac40be2dd0c9716cb609951a9
chen <xxx@example.com>
2020-04-27 11:19:05 +0800

    地图 演示 重写

--- testmap/src/main/java/com/example/testmap/MainActivity.java
+++ testmap/src/main/java/com/example/testmap/MainActivity.java
@@ -1,13 +1,9 @@
 package com.example.testmap;
 
-import android.Manifest;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.BDLocation;
@@ -18,15 +14,16 @@ import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.MyLocationConfiguration;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.map.OverlayOptions;
 import com.baidu.mapapi.model.LatLng;
 import com.baidu.mapapi.search.core.SearchResult;
 import com.baidu.mapapi.search.poi.OnGetPoiSearchResultListener;
+import com.baidu.mapapi.search.poi.PoiCitySearchOption;
 import com.baidu.mapapi.search.poi.PoiDetailResult;
 import com.baidu.mapapi.search.poi.PoiDetailSearchResult;
 import com.baidu.mapapi.search.poi.PoiIndoorResult;
-import com.baidu.mapapi.search.poi.PoiNearbySearchOption;
 import com.baidu.mapapi.search.poi.PoiResult;
 import com.baidu.mapapi.search.poi.PoiSearch;
 import com.baidu.mapapi.search.route.BikingRouteResult;
@@ -41,78 +38,52 @@ import com.baidu.mapapi.search.route.WalkingRoutePlanOption;
 import com.baidu.mapapi.search.route.WalkingRouteResult;
 import com.example.testmap.overlayutil.PoiOverlay;
 import com.example.testmap.overlayutil.WalkingRouteOverlay;
-import com.github.dfqin.grantor.PermissionListener;
-import com.github.dfqin.grantor.PermissionsUtil;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button mRouteplan;
+    private MapView mBmapView;
     private BaiduMap mBaiduMap;
     private LocationClient mLocationClient;
-    OnGetPoiSearchResultListener listener = new OnGetPoiSearchResultListener() {
-        private int totalPage;
-
+    private PoiSearch mPoiSearch;
+    OnGetPoiSearchResultListener onGetPoiSearchResultListener = new OnGetPoiSearchResultListener() {
         @Override
         public void onGetPoiResult(PoiResult poiResult) {
-            Log.d(TAG, "onGetPoiResult: ");
-
-            if (poiResult.error == SearchResult.ERRORNO.NO_ERROR) {// 检索结果正常返回
+            if (poiResult.error == SearchResult.ERRORNO.NO_ERROR) {
                 mBaiduMap.clear();
+
+                //创建PoiOverlay对象
                 PoiOverlay poiOverlay = new PoiOverlay(mBaiduMap);
-                poiOverlay.setData(poiResult);// 设置POI数据
-                mBaiduMap.setOnMarkerClickListener(poiOverlay);
-                poiOverlay.addToMap();// 将所有的overlay添加到地图上
+
+                //设置Poi检索数据
+                poiOverlay.setData(poiResult);
+
+                //将poiOverlay添加至地图并缩放至合适级别
+                poiOverlay.addToMap();
                 poiOverlay.zoomToSpan();
-                //
-                totalPage = poiResult.getTotalPageNum();// 获取总分页数
-                Toast.makeText(
-                        MainActivity.this,
-                        "总共查到" + poiResult.getTotalPoiNum() + "个兴趣点, 分为"
-                                + totalPage + "页", Toast.LENGTH_SHORT).show();
             }
-
-
         }
-
         @Override
         public void onGetPoiDetailResult(PoiDetailSearchResult poiDetailSearchResult) {
 
-            Log.d(TAG, "onGetPoiDetailResult: ");
         }
-
         @Override
         public void onGetPoiIndoorResult(PoiIndoorResult poiIndoorResult) {
 
-            Log.d(TAG, "onGetPoiIndoorResult: ");
         }
-
         //废弃
         @Override
         public void onGetPoiDetailResult(PoiDetailResult poiDetailResult) {
 
-            Log.d(TAG, "onGetPoiDetailResult: ");
         }
     };
-    private PoiSearch mPoiSearch;
     private RoutePlanSearch mSearch;
-    private Button mClick;
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.click:
-                // TODO 20/04/25
-                initRoutePlan();
-                break;
-
-        }
-    }
 
     public class MyLocationListener extends BDAbstractLocationListener {
         @Override
         public void onReceiveLocation(BDLocation location) {
             //mapView 销毁后不在处理新接收的位置
-            Log.d(TAG, "onReceiveLocation: ");
-            if (location == null || mBaiduMap == null) {
+            if (location == null || mBmapView == null){
                 return;
             }
             MyLocationData locData = new MyLocationData.Builder()
@@ -123,20 +94,127 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             mBaiduMap.setMyLocationData(locData);
         }
     }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+//        initMarker();
 
-    OnGetRoutePlanResultListener onGetRoutePlanResultListener = new OnGetRoutePlanResultListener() {
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mPoiSearch = PoiSearch.newInstance();
+        mPoiSearch.setOnGetPoiSearchResultListener(onGetPoiSearchResultListener);
+
+    }
+
+    private void clickPOI() {
+
+        mPoiSearch.searchInCity(new PoiCitySearchOption()
+                .city("北京") //必填
+                .keyword("美食") //必填
+                .pageNum(10));
+    }
+
+    private void initMarker() {
+
+        //定义Maker坐标点
+        LatLng point = new LatLng(39.963175, 116.400244);
+//构建Marker图标
+        BitmapDescriptor bitmap = BitmapDescriptorFactory
+                .fromResource(R.drawable.icon_mark);
+//构建MarkerOption，用于在地图上添加Marker
+        OverlayOptions option = new MarkerOptions()
+                .position(point)
+                .icon(bitmap);
+//在地图上添加Marker，并显示
+        mBaiduMap.addOverlay(option);
+    }
+
+    private void initView() {
+        mRouteplan = (Button) findViewById(R.id.routeplan);
+        mRouteplan.setOnClickListener(this);
+        mBmapView = (MapView) findViewById(R.id.bmapView);
+
+        mBaiduMap = mBmapView.getMap();
+        //普通地图 ,mBaiduMap是地图控制器对象
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+
+        //开启热力图
+//        mBaiduMap.setBaiduHeatMapEnabled(true);
+        // 地图的定位图层
+        mBaiduMap.setMyLocationEnabled(true);
+
+//        mBaiduMap.setMyLocationConfiguration(new MyLocationConfiguration(MyLocationConfiguration.LocationMode.FOLLOWING,false,null));
+
+
+
+        //定位初始化
+        mLocationClient = new LocationClient(this);
+
+//通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(1000);
+
+//设置locationClientOption
+        mLocationClient.setLocOption(option);
+
+//注册LocationListener监听器
+        MyLocationListener myLocationListener = new MyLocationListener();
+        mLocationClient.registerLocationListener(myLocationListener);
+//开启地图定位图层
+        mLocationClient.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mBmapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mBmapView.onDestroy();
+
+        mSearch.destroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.routeplan:
+                // TODO 20/04/27
+
+//              clickPOI();
+                initRoutePlan();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+
+    OnGetRoutePlanResultListener listener = new OnGetRoutePlanResultListener() {
         @Override
         public void onGetWalkingRouteResult(WalkingRouteResult walkingRouteResult) {
             //创建WalkingRouteOverlay实例
-            if (walkingRouteResult.error == SearchResult.ERRORNO.NO_ERROR) {
-                WalkingRouteOverlay overlay = new WalkingRouteOverlay(mBaiduMap);
-                if (walkingRouteResult.getRouteLines() != null && walkingRouteResult.getRouteLines().size() > 0) {
-                    //获取路径规划数据,(以返回的第一条数据为例)
-                    //为WalkingRouteOverlay实例设置路径数据
-                    overlay.setData(walkingRouteResult.getRouteLines().get(0));
-                    //在地图上绘制WalkingRouteOverlay
-                    overlay.addToMap();
-                }
+            WalkingRouteOverlay overlay = new WalkingRouteOverlay(mBaiduMap);
+            if (walkingRouteResult.getRouteLines().size() > 0) {
+                //获取路径规划数据,(以返回的第一条数据为例)
+                //为WalkingRouteOverlay实例设置路径数据
+                overlay.setData(walkingRouteResult.getRouteLines().get(0));
+                //在地图上绘制WalkingRouteOverlay
+                overlay.addToMap();
             }
         }
 
@@ -166,156 +244,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     };
-    String[] permissions = {
-            Manifest.permission.ACCESS_NETWORK_STATE,
-            Manifest.permission.INTERNET,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_WIFI_STATE
-
-    };
-    private MapView mMapView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        PermissionsUtil.requestPermission(this, new PermissionListener() {
-            @Override
-            public void permissionGranted(@NonNull String[] permission) {
-                initView();
-            }
-
-            @Override
-            public void permissionDenied(@NonNull String[] permission) {
-
-            }
-        }, permissions);
-
-    }
-
-    private static final String TAG = "MainActivity";
-
-    public void initMark() {
-
-        //定义Maker坐标点
-        LatLng point = new LatLng(39.963175, 116.400244);
-//构建Marker图标
-        BitmapDescriptor bitmap = BitmapDescriptorFactory
-                .fromResource(R.drawable.icon_mark);
-//构建MarkerOption，用于在地图上添加Marker
-        OverlayOptions option = new MarkerOptions()
-                .position(point)
-                .icon(bitmap);
-//在地图上添加Marker，并显示
-        mBaiduMap.addOverlay(option);
-    }
-
-    public void initPOI() {
-        mPoiSearch = PoiSearch.newInstance();
-        mPoiSearch.setOnGetPoiSearchResultListener(listener);
-//        mPoiSearch.searchInCity(new PoiCitySearchOption()
-//                .city("北京") //必填
-//                .keyword("餐厅") //必填
-//                .pageNum(10));
-
-        mPoiSearch.searchNearby(new PoiNearbySearchOption()
-                .location(new LatLng(39.915446, 116.403869))
-                .radius(10000)
-                .keyword("餐厅")
-
-                .pageNum(10));
-    }
-
-    private void initView() {
-
-        Log.d(TAG, "initView: ");
-        if (mMapView == null) {
-            mMapView = (MapView) findViewById(R.id.bmapView);
-        }
-        mBaiduMap = mMapView.getMap();
-        mBaiduMap.setMyLocationEnabled(true);
-//        mBaiduMap.setMyLocationConfiguration(new MyLocationConfiguration(MyLocationConfiguration.LocationMode.FOLLOWING,false,null));
-
-
-        initMark();
-        initPOI();
-
-        //定位初始化
-        mLocationClient = new LocationClient(this);
-
-//通过LocationClientOption设置LocationClient相关参数
-        LocationClientOption option = new LocationClientOption();
-        option.setOpenGps(true); // 打开gps
-        option.setCoorType("bd09ll"); // 设置坐标类型
-        option.setScanSpan(1000);
-
-//设置locationClientOption
-        mLocationClient.setLocOption(option);
-
-//注册LocationListener监听器
-        MyLocationListener myLocationListener = new MyLocationListener();
-        mLocationClient.registerLocationListener(myLocationListener);
-//开启地图定位图层
-        mLocationClient.start();
-
-        mClick = (Button) findViewById(R.id.click);
-        mClick.setOnClickListener(this);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
-        if (mMapView != null)
-            mMapView.onResume();
-
-
-        initPOI();
-
-
-    }
-
+    /**
+     * 路径规划
+     */
     private void initRoutePlan() {
+
         mSearch = RoutePlanSearch.newInstance();
-        mSearch.setOnGetRoutePlanResultListener(onGetRoutePlanResultListener);
-
-
-        PlanNode stNode = PlanNode.withCityNameAndPlaceName("北京", "西二旗地铁站");
+        mSearch.setOnGetRoutePlanResultListener(listener);
+        PlanNode stNode = PlanNode.withCityNameAndPlaceName("北京", "天安门");
         PlanNode enNode = PlanNode.withCityNameAndPlaceName("北京", "百度科技园");
 
+        // 开始 规划
         mSearch.walkingSearch((new WalkingRoutePlanOption())
                 .from(stNode)
                 .to(enNode));
-
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
-        if (mMapView != null)
-            mMapView.onPause();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mLocationClient.stop();
-        if (mBaiduMap != null)
-            mBaiduMap.setMyLocationEnabled(false);
-        if (mMapView != null)
-            mMapView.onDestroy();
-        mMapView = null;
-
-
-        mPoiSearch.destroy();
-
-        mSearch.destroy();
     }
 }
